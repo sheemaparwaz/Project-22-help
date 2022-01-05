e13517d35e82dc08d1f315f1cfbc68ebab1d4a53
Sheema Parwaz <SheemaParwaz@gmail.com>
2022-01-05 10:06:50 +0530

    debugged

--- PlayerArcher.js
+++ PlayerArcher.js
@@ -8,7 +8,7 @@ class PlayerArcher {
   //  this.body = Matter.Bodies(x, y, width, height, options);
   //  this.body = Matter.Bodies.rect(x, y, width, height);
   //  this.body = Matter.rectangle(x, y, width, height, options);
-  //  this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+    this.body = Matter.Bodies.rectangle(x, y, width, height, options);
 
     this.width = width;
     this.height = height;
